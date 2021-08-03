55619e87151d8436759a199d16864c6db27239ec
ss.yulin <ss.yulin@gitlab.com>
2021-08-03 10:44:36 +0300

    testMultiplication - 1

--- tdd/tests/main.cpp
+++ tdd/tests/main.cpp
@@ -10,9 +10,31 @@ public:
 
 };
 
-TEST_F(MoneyTest, testMoney)
+class Dollar
 {
-    EXPECT_EQ(1+1, 2);
+public:
+    Dollar(int amount): m_amount(amount)
+    {
+    }
+
+    void multipledBy(int multiplier)
+    {
+        m_amount = m_amount * multiplier;
+    }
+    int amount() const noexcept
+    {
+        return m_amount;
+    }
+private:
+    int m_amount;
+
+};
+
+TEST_F(MoneyTest, testMultiplication)
+{
+    Dollar five(5);
+    five.multipledBy(2);
+    EXPECT_EQ(10, five.amount());
 }
 
 int main(int argc, char** argv)
