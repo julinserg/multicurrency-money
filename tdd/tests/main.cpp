0d6e7dd1d964994c89f9231f403961e8e066df25
ss.yulin <ss.yulin@gitlab.com>
2021-08-03 12:44:59 +0300

    Глава 5 - Поговорим о франках

--- tdd/tests/main.cpp
+++ tdd/tests/main.cpp
@@ -31,10 +31,35 @@ public:
 
 private:
     const int m_amount;
-
 };
 
 bool operator == (const Dollar& object1, const Dollar& object2)
+{
+    return object1.equals(object2);
+}
+
+class Franc
+{
+public:
+    Franc(int amount): m_amount(amount)
+    {
+    }
+
+    std::unique_ptr<Franc> multipliedBy(int multiplier)
+    {
+        return std::make_unique<Franc>(m_amount * multiplier);
+    }
+
+    bool equals(const Franc& object) const
+    {
+        return object.m_amount == m_amount;
+    }
+
+private:
+    const int m_amount;
+};
+
+bool operator == (const Franc& object1, const Franc& object2)
 {
     return object1.equals(object2);
 }
@@ -53,6 +78,13 @@ TEST_F(MoneyTest, testEquality)
     Dollar six(6);
     EXPECT_EQ(five1.equals(five2), true);
     EXPECT_EQ(five1.equals(six), false);
+}
+
+TEST_F(MoneyTest, testFrancMultiplication)
+{
+    Franc five(5);
+    EXPECT_EQ(Franc(10), *five.multipliedBy(2));
+    EXPECT_EQ(Franc(15), *five.multipliedBy(3));
 }
 
 int main(int argc, char** argv)
